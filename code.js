9b80357d595cac7b7e31f2cc960becb76261c75c
Fredi <Fredi1574@gmail.com>
2021-08-14 16:06:22 +0300

    working v1

--- code.js
+++ code.js
@@ -4,6 +4,7 @@ const container = document.getElementById("mainContainer");
 
 addButton.addEventListener("click", () => {
   if (textBox.value === "") {
+    // if the textbox is empty
     textBox.classList.add("shake");
     textBox.placeholder = "You need to enter a task!";
     setTimeout(() => {
@@ -14,11 +15,16 @@ addButton.addEventListener("click", () => {
   }
 
   let newTask = document.createElement("li");
-  /////////////////////////
-  newTask.classList.add("tasks");
-  newTask.value = textBox.innerText;
+  let deleteButton = document.createElement("button");
+  deleteButton.addEventListener("click", () => {
+    newTask.remove();
+  });
 
+  deleteButton.innerHTML = "X";
+  newTask.innerHTML = textBox.value;
+
+  newTask.appendChild(deleteButton);
   container.appendChild(newTask);
-  /////////////////////////
+
   textBox.value = "";
 });
